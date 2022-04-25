32b299a2b75ab2f9ba43a40f909f354c6b2de4f3
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2022-04-25 18:15:53 +0300

    test(cart-widget): add tests

--- src/shared/CartWidget/CartWidget.spec.tsx
+++ src/shared/CartWidget/CartWidget.spec.tsx
@@ -1,5 +1,37 @@
+import React from "react";
+import { fireEvent } from '@testing-library/react'
+
 import { CartWidget } from './CartWidget'
 
 describe("CartWidget", () => {
-  it.todo("renders correctly")
-})
+  it("renders correctly", () => {
+    const { container } = renderWithRouter(() => <CartWidget />, "/");
+
+    expect(container).toBeInTheDocument();
+  });
+
+  it("shows the amount of products in the cart", () => {
+    const mockUseCart = jest.fn().mockImplementation(() => ({
+      products: [
+        {
+          name: "Product foo",
+          price: 0,
+          image: "foo.png",
+        },
+      ],
+    }));
+
+    const { container } = renderWithRouter(() => <CartWidget useCartHook={mockUseCart} />, "/");
+
+    expect(container.innerHTML).toMatch("1");
+    expect(mockUseCart).toBeCalledTimes(1);
+  });
+
+  it("navigates to cart summary page on click", () => {
+    const { getByRole, history } = renderWithRouter(() => <CartWidget />, "/");
+
+    fireEvent.click(getByRole("link"));
+
+    expect(history.location.pathname).toBe("/cart");
+  });
+});
