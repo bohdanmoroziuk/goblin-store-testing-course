a3dc553d4cf4382cda9f652be2384367b03ff385
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2022-04-27 20:08:26 +0300

    test(order-summary): add tests

--- src/OrderSummary/OrderSummary.spec.tsx
+++ src/OrderSummary/OrderSummary.spec.tsx
@@ -1,15 +1,69 @@
 import React from 'react'
+import { fireEvent, render } from "@testing-library/react";
+
+import { Loader } from 'shared/Loader';
+import { OrderSummary } from "./OrderSummary";
+
+jest.mock("../shared/Loader.tsx", () => ({
+  Loader: jest.fn(() => null),
+}))
 
 describe("OrderSummary", () => {
+  afterEach(jest.clearAllMocks);
+
   describe("while order data being loaded", () => {
-    it.todo("renders loader")
+    it("renders loader", () => {
+      const mockUseOrder = jest.fn(() => ({
+        isLoading: true,
+        order: undefined,
+      }))
+
+      render(<OrderSummary useOrderHook={mockUseOrder} />)
+
+      expect(Loader).toHaveBeenCalled();
+    })
   }) 
 
   describe("when order is loaded", () => {
-    it.todo("renders order info")
+    const mockUseOrder = jest.fn(() => ({
+      isLoading: false,
+      order: {
+        products: [
+          {
+            name: "Foo",
+            price: 55,
+            image: "image.png"
+          }
+        ]
+      }
+    }))
+
+    it("renders order info", () => {
+
+      const { container } = renderWithRouter(() => <OrderSummary useOrderHook={mockUseOrder} />) 
+
+      expect(container.innerHTML).toMatch("Foo")
+    })
+
+    it("navigates to main page on button click", () => {
+      const { getByText, history } = renderWithRouter(() => <OrderSummary useOrderHook={mockUseOrder} />)
+
+      fireEvent.click(getByText("Back to the store"))
+      
+      expect(history.location.pathname).toBe("/")
+    })
   }) 
 
   describe("with error", () => {
-    it.todo("renders error info")
+    const mockUseOrder = jest.fn(() => ({
+      isLoading: false,
+      order: undefined,
+    }))
+
+    it("renders error message", () => {
+      const { container } = render(<OrderSummary useOrderHook={mockUseOrder} />)
+
+      expect(container.innerHTML).toMatch("Couldn't load order info.")
+    })
   }) 
 })
