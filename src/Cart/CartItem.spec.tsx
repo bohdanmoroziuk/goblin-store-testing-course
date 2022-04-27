d472438377b489e4ab4b818d06e5c9b8a3d063d3
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2022-04-27 09:05:29 +0300

    test(cart-item): add tests

--- src/Cart/CartItem.spec.tsx
+++ src/Cart/CartItem.spec.tsx
@@ -1,9 +1,37 @@
+import { fireEvent } from '@testing-library/react';
 import React from 'react'
+import { Product } from 'shared/types'
+import { CartItem } from './CartItem';
 
 describe("CartItem", () => {
-  it.todo("renders correctly")
+  const product: Product = {
+    name: "Foo",
+    price: 55,
+    image: "/images/basic_spear.png"
+  };
+
+  const mockRemoveFromCart = jest.fn();
+
+  it("renders correctly", () => {
+    const { container, getByAltText } = renderWithRouter(
+      () => <CartItem product={product} removeFromCart={mockRemoveFromCart} />
+    );
+
+    expect(container.innerHTML).toMatch(product.name);
+    expect(container.innerHTML).toMatch(product.price.toString());
+    expect(getByAltText(product.name)).toHaveAttribute("src", product.image);
+    expect(container).toMatchSnapshot();
+  })
 
   describe("on 'Remove item' click", () => {
-    it.todo("calls passed in function")
+    it("removes the item from the cart", () => {
+      const { getByText } = renderWithRouter(
+        () => <CartItem product={product} removeFromCart={mockRemoveFromCart} />
+      );
+
+      fireEvent.click(getByText("Remove"));
+
+      expect(mockRemoveFromCart).toBeCalledWith(product);
+    })
   })
 })
