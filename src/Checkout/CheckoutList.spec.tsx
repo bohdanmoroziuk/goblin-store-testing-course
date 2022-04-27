e336857acda0e8fc248294165378c1fbb55b6de8
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2022-04-27 09:19:41 +0300

    test(checkout-list): add test

--- src/Checkout/CheckoutList.spec.tsx
+++ src/Checkout/CheckoutList.spec.tsx
@@ -1,5 +1,28 @@
 import React from 'react';
+import { render } from '@testing-library/react';
+
+import { Product } from "../shared/types";
+import { CheckoutList } from "./CheckoutList";
 
 describe("CheckoutList", () => {
-  it.todo("renders list of products")
+  const products: Product[] = [
+    {
+      name: "Foo",
+      price: 55,
+      image: "/images/basic_spear.png",
+    },
+    {
+      name: "Bar",
+      price: 10,
+      image: "/images/broad_sword.png",
+    },
+  ];
+
+  it("renders list of products", () => {
+    const { container } = render(<CheckoutList products={products} />)
+
+    expect(container).toMatchSnapshot();
+    expect(container.innerHTML).toMatch(products[0].name);
+    expect(container.innerHTML).toMatch(products[1].name);
+  })
 })
